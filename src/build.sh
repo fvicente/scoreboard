8412dd5399c56f52bb08f957d946ab1509c2c6af
fvicente <fvicente@mwneo.com>
2016-02-22 23:59:46 -0300

    Final version

--- src/build.sh
+++ src/build.sh
@@ -4,6 +4,7 @@ avr-gcc -mmcu=attiny45 -Wl,-Map=scoreboard.map main.o const.o     -o scoreboard.
 avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature  scoreboard.elf scoreboard.hex
 avr-objcopy -j .eeprom --set-section-flags=.eeprom="alloc,load" --change-section-lma .eeprom=0 --no-change-warnings -O ihex scoreboard.elf scoreboard.eep || exit 0
 avr-objdump -h -S scoreboard.elf > scoreboard.lss
+avr-size -C --mcu=attiny45 scoreboard.elf
 rm *.o
 rm *.o.d
 rm scoreboard.eep
--- src/upload.sh
+++ src/upload.sh
@@ -4,3 +4,5 @@ PORT=/dev/ttyUSB0
 avrdude -F -P $PORT -p attiny45 -c avrisp -b 19200 -U flash:w:scoreboard.hex
 # fuses
 #avrdude -F -P /dev/ttyUSB0 -p t45 -c avrisp -Ulfuse:w:0xce:m -Uhfuse:w:0xdf:m -Uefuse:w:0xff:m
+# WARNING: next line will set the fuses with PB5 as I/O port (won't be able to program again unless you reset the fuses with HV programmer)
+#avrdude -F -P $PORT -p attiny45 -c avrisp -b 19200 -U flash:w:scoreboard.hex -Ulfuse:w:0xce:m -Uhfuse:w:0x5f:m -Uefuse:w:0xff:m
